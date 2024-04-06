cabda2f7d9c470e8cfec79cc24a0b58efff8690a
unknown <amanyadav16544@gmail.com>
2024-04-06 12:23:20 +0530

    tailwind css setup is done and initlisied

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-zinc-900'>App</div>
+    <div className='w-full h-screen bg-zinc-900 text-white'>
+      App
+    </div>
   )
 }
 
